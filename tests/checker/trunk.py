e9f920c99b419b99e88534317817c0ab4a64a1e6
hao.long <hoolongvfx@dgmail.com>
2017-02-01 23:13:33 +0800

    clean

--- tests/checker/trunk.py
+++ tests/checker/trunk.py
@@ -6,7 +6,6 @@ import re
 
 def sendComment(message_):
     pr_url = os.environ['ghprbPullLink']
-    # pr_url = "https://github.com/tmikota/、/pull/56"
     pr_url = pr_url.replace("github.com/", "api.github.com/repos/")
     pr_url = pr_url.replace("pull", "pulls")
     repo = pr_url.split("/")
